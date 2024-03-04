9831ff4dc00f7227ab66e45cda0eb647c5c372c6
Youssef-joe <joe92228@gmail.com>
2024-03-05 00:07:52 +0200

    initial commit

---
+++ script/word-list.js
@@ -0,0 +1,127 @@
+const wordList = [
+    {
+        word: 'youssef',
+        hint: 'who made this game!'
+    },
+    {
+        word: 'mans',
+        hint: 'hossam hosny vibes'
+    },
+    {
+        word: 'adam',
+        hint:`the one who didn't attend`
+    },
+    {
+        word: `eyad`,
+        hint: `the youngest engineer`
+    },
+    {
+        word: `fawaz`,
+        hint: `the most funny engine`
+    },
+    {
+        word: `ramez`,
+        hint: `history spanish man`
+    },
+    {
+        word: `nour`,
+        hint: `the weirdest lady`
+    },
+    {
+        word: `dahlob`,
+        hint: `the most cultured farmer scientest`
+    },
+    {
+        word: `huda`,
+        hint: `the cutest girl in the team`
+    },
+    {
+        word: `ahmed`,
+        hint: `the most angry member in the team`
+    },
+    {
+        word: `yassin`,
+        hint: `the engineer who hates us`
+    },
+    {
+        word: `zizo`,
+        hint:  `أروق دماغ في التيم`
+    },
+    {
+        word: `sanad`,
+        hint: `the toxic engineer`
+    },
+    {
+        word: `menna`,
+        hint: `البنت الشطوية بتاعة الانجلش`
+    },
+    {
+        word: `saif`,
+        hint: `نعيم الجهل`
+    },
+    {
+        word: `salma`,
+        hint: `she's a software engineer but casher`
+    },
+    {
+        word: `bakr`,
+        hint: `the most funny and wise member at the same time`
+    },
+    {
+        word: `fatma`,
+        hint: `the most loveable casher`
+    },
+    {
+        word: `habiba`,
+        hint: `the youngest casher`
+    },
+    {
+        word: `mahran`,
+        hint: `the most talkative one`
+
+    },
+    {
+        word: `nahla`,
+        hint: `رف الخمسين`
+    },
+    {
+        word: `mordy`,
+        hint: `daredevil (mordok)`
+    },
+    {
+        word: `abdullah`,
+        hint: `he's a time traveler from the old ages`
+    },
+    {
+        word: `donia`,
+        hint: `the girl with no voice`
+    },
+    {
+        word: `habiba`,
+        hint: `the tiktok photographer`
+    },
+    {
+        word: `aya`,
+        hint: `ن`
+    },
+    {
+        word:`maggi`,
+        hint: `واسطة نور`
+    },
+    {
+        word: `shahd`,
+        hint: `birthday girl`
+    },
+    {
+        word: 'gamila',
+        hint: `اسم على مسمى`
+    },
+    {
+        word: `mostafa`,
+        hint: `-18`
+    },
+    {
+        word: `yasmine`,
+        hint: `we all love her drawing art`
+    }
+]
